9d573d6c0d4d3baf6551631a614af832e8e9cc94
teejay327 <mrenergy@hotmail.com>
2024-12-08 23:34:35 +1000

    Added sidebar

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,18 @@
+const ProjectsSidebar = () => {
+
+    return (
+      <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50">
+        <h2>Your projects</h2>
+        <div>
+          <button>
+            + Add project
+          </button>
+        </div>
+        <ul>
+
+        </ul>
+      </aside>
+    )
+};
+
+export default ProjectsSidebar;
