ea79e3caea7939783ea945e4c6006e2ed44f67e6
teejay327 <mrenergy@hotmail.com>
2025-01-02 23:26:49 +1000

    Changed font

--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,4 +1,4 @@
-import Button from './Button.jsx';
+import Button from './Button.jsx'
 import NoProjectImage from '../assets/no-projects.png';
 
 const NoProjectSelected = () => {
@@ -6,10 +6,10 @@ const NoProjectSelected = () => {
   return (
     <div className="mt-24 text-center w-2/3">
       <img className="w-16 h-16 object-contain mx-auto" src={ NoProjectImage }/>
-      <h2 className=" my-4 text-xl font-bold text-stone-500">No project selected</h2>
+      <h2 className=" my-4 text-xl font-bold text-stone-600">No project selected</h2>
       <p className="text-stone-400 mb-4">Select a project or get started with a new one</p>
-      <p className="mt-8">
-        <Button >Create a new project</Button>
+      <p classNamer="mt-8">
+        <Button>Create a new project</Button>
       </p>
     </div>
   )
